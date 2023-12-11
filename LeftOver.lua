f546a42d2731d15c3e16c11611090271638a972e
psswaldo <152012526+psswaldo@users.noreply.github.com>
2023-12-11 12:16:10 +0100

    Create LeftOver.lua

---
+++ LeftOver.lua
@@ -0,0 +1,253 @@
+local TabBtnHolder_2 = Instance.new("Frame")
+local TabBtn_2 = Instance.new("TextButton")
+local UIPadding_4 = Instance.new("UIPadding")
+local TabBtnImage_2 = Instance.new("ImageLabel")
+
+
+
+local Toggle_1 = Instance.new("Frame")
+local ToggleText_1 = Instance.new("TextLabel")
+local UIPadding_5 = Instance.new("UIPadding")
+local ToggleHolder_1 = Instance.new("TextButton")
+local Toggler_1 = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+
+local Toggle_2 = Instance.new("Frame")
+local ToggleText_2 = Instance.new("TextLabel")
+local UIPadding_7 = Instance.new("UIPadding")
+local ToggleHolder_2 = Instance.new("TextButton")
+local Toggler_2 = Instance.new("Frame")
+local UICorner_3 = Instance.new("UICorner")
+local Label_1 = Instance.new("Frame")
+local LabelText_1 = Instance.new("TextLabel")
+
+local Slider_1 = Instance.new("Frame")
+local SliderText_1 = Instance.new("TextLabel")
+local UIPadding_9 = Instance.new("UIPadding")
+local SliderValue_1 = Instance.new("TextLabel")
+local UIPadding_10 = Instance.new("UIPadding")
+local SliderHolder_1 = Instance.new("Frame")
+local SliderBtn_1 = Instance.new("TextButton")
+local SliderInner_1 = Instance.new("Frame")
+local UICorner_4 = Instance.new("UICorner")
+local UICorner_5 = Instance.new("UICorner")
+
+-- Properties:
+
+
+
+
+
+TabBtnHolder_2.Name = "TabBtnHolder"
+TabBtnHolder_2.Parent = Tabs_1
+TabBtnHolder_2.BackgroundColor3 = Color3.fromRGB(255,255,255)
+TabBtnHolder_2.BackgroundTransparency = 1
+TabBtnHolder_2.BorderColor3 = Color3.fromRGB(0,0,0)
+TabBtnHolder_2.BorderSizePixel = 0
+TabBtnHolder_2.Size = UDim2.new(0, 128,0, 25)
+
+TabBtn_2.Name = "TabBtn"
+TabBtn_2.Parent = TabBtnHolder_2
+TabBtn_2.Active = true
+TabBtn_2.BackgroundColor3 = Color3.fromRGB(255,255,255)
+TabBtn_2.BackgroundTransparency = 1
+TabBtn_2.BorderColor3 = Color3.fromRGB(0,0,0)
+TabBtn_2.BorderSizePixel = 0
+TabBtn_2.Size = UDim2.new(0, 128,0, 25)
+TabBtn_2.Font = Enum.Font.Gotham
+TabBtn_2.Text = "Settings"
+TabBtn_2.TextColor3 = Color3.fromRGB(96,73,78)
+TabBtn_2.TextSize = 13
+TabBtn_2.TextXAlignment = Enum.TextXAlignment.Left
+
+UIPadding_4.Parent = TabBtn_2
+UIPadding_4.PaddingLeft = UDim.new(0,33)
+
+TabBtnImage_2.Name = "TabBtnImage"
+TabBtnImage_2.Parent = TabBtnHolder_2
+TabBtnImage_2.BackgroundColor3 = Color3.fromRGB(255,255,255)
+TabBtnImage_2.BackgroundTransparency = 1
+TabBtnImage_2.BorderColor3 = Color3.fromRGB(0,0,0)
+TabBtnImage_2.BorderSizePixel = 0
+TabBtnImage_2.Position = UDim2.new(0.0703125, 0,0.119999997, 0)
+TabBtnImage_2.Size = UDim2.new(0, 18,0, 18)
+TabBtnImage_2.Image = "rbxassetid://15606415939"
+TabBtnImage_2.ImageColor3 = Color3.fromRGB(96,73,78)
+
+
+
+
+
+
+
+Toggle_1.Name = "Toggle"
+Toggle_1.Parent = Items_1
+Toggle_1.BackgroundColor3 = Color3.fromRGB(255,255,255)
+Toggle_1.BackgroundTransparency = 1
+Toggle_1.BorderColor3 = Color3.fromRGB(0,0,0)
+Toggle_1.BorderSizePixel = 0
+Toggle_1.Size = UDim2.new(0, 262,0, 27)
+
+ToggleText_1.Name = "ToggleText"
+ToggleText_1.Parent = Toggle_1
+ToggleText_1.BackgroundColor3 = Color3.fromRGB(255,255,255)
+ToggleText_1.BackgroundTransparency = 1
+ToggleText_1.BorderColor3 = Color3.fromRGB(0,0,0)
+ToggleText_1.BorderSizePixel = 0
+ToggleText_1.Size = UDim2.new(0, 192,0, 27)
+ToggleText_1.Font = Enum.Font.Gotham
+ToggleText_1.Text = "Toggle Off"
+ToggleText_1.TextColor3 = Color3.fromRGB(172,114,125)
+ToggleText_1.TextSize = 13
+ToggleText_1.TextXAlignment = Enum.TextXAlignment.Left
+
+UIPadding_5.Parent = ToggleText_1
+UIPadding_5.PaddingLeft = UDim.new(0,9)
+
+ToggleHolder_1.Name = "ToggleHolder"
+ToggleHolder_1.Parent = Toggle_1
+ToggleHolder_1.BackgroundColor3 = Color3.fromRGB(255,255,255)
+ToggleHolder_1.BackgroundTransparency = 1
+ToggleHolder_1.BorderColor3 = Color3.fromRGB(0,0,0)
+ToggleHolder_1.BorderSizePixel = 0
+ToggleHolder_1.Position = UDim2.new(0.767175555, 0,0.0740740746, 0)
+ToggleHolder_1.Size = UDim2.new(0, 60,0, 22)
+ToggleHolder_1.Text = ""
+ToggleHolder_1.TextColor3 = Color3.fromRGB(27,42,53)
+ToggleHolder_1.TextSize = 8
+
+Toggler_1.Name = "Toggler"
+Toggler_1.Parent = ToggleHolder_1
+Toggler_1.BackgroundColor3 = Color3.fromRGB(96,73,78)
+Toggler_1.BorderColor3 = Color3.fromRGB(0,0,0)
+Toggler_1.BorderSizePixel = 0
+Toggler_1.Position = UDim2.new(0.516666651, 0,0, 0)
+Toggler_1.Size = UDim2.new(0, 30,0, 22)
+
+UICorner_2.Parent = Toggler_1
+
+
+
+Toggle_2.Name = "Toggle"
+Toggle_2.Parent = Items_1
+Toggle_2.BackgroundColor3 = Color3.fromRGB(255,255,255)
+Toggle_2.BackgroundTransparency = 1
+Toggle_2.BorderColor3 = Color3.fromRGB(0,0,0)
+Toggle_2.BorderSizePixel = 0
+Toggle_2.Size = UDim2.new(0, 262,0, 27)
+
+ToggleText_2.Name = "ToggleText"
+ToggleText_2.Parent = Toggle_2
+ToggleText_2.BackgroundColor3 = Color3.fromRGB(255,255,255)
+ToggleText_2.BackgroundTransparency = 1
+ToggleText_2.BorderColor3 = Color3.fromRGB(0,0,0)
+ToggleText_2.BorderSizePixel = 0
+ToggleText_2.Size = UDim2.new(0, 192,0, 27)
+ToggleText_2.Font = Enum.Font.Gotham
+ToggleText_2.Text = "Toggle On"
+ToggleText_2.TextColor3 = Color3.fromRGB(172,114,125)
+ToggleText_2.TextSize = 13
+ToggleText_2.TextXAlignment = Enum.TextXAlignment.Left
+
+UIPadding_7.Parent = ToggleText_2
+UIPadding_7.PaddingLeft = UDim.new(0,9)
+
+ToggleHolder_2.Name = "ToggleHolder"
+ToggleHolder_2.Parent = Toggle_2
+ToggleHolder_2.BackgroundColor3 = Color3.fromRGB(255,255,255)
+ToggleHolder_2.BackgroundTransparency = 1
+ToggleHolder_2.BorderColor3 = Color3.fromRGB(0,0,0)
+ToggleHolder_2.BorderSizePixel = 0
+ToggleHolder_2.Position = UDim2.new(0.76700002, 0,0.074000001, 0)
+ToggleHolder_2.Size = UDim2.new(0, 60,0, 22)
+ToggleHolder_2.Text = ""
+ToggleHolder_2.TextColor3 = Color3.fromRGB(27,42,53)
+ToggleHolder_2.TextSize = 8
+
+Toggler_2.Name = "Toggler"
+Toggler_2.Parent = ToggleHolder_2
+Toggler_2.BackgroundColor3 = Color3.fromRGB(146,112,118)
+Toggler_2.BorderColor3 = Color3.fromRGB(0,0,0)
+Toggler_2.BorderSizePixel = 0
+Toggler_2.Size = UDim2.new(0, 30,0, 22)
+
+UICorner_3.Parent = Toggler_2
+
+
+
+
+
+Slider_1.Name = "Slider"
+Slider_1.Parent = Items_1
+Slider_1.BackgroundColor3 = Color3.fromRGB(255,255,255)
+Slider_1.BackgroundTransparency = 1
+Slider_1.BorderColor3 = Color3.fromRGB(0,0,0)
+Slider_1.BorderSizePixel = 0
+Slider_1.Position = UDim2.new(0, 0,0.483108103, 0)
+Slider_1.Size = UDim2.new(0, 262,0, 43)
+
+SliderText_1.Name = "SliderText"
+SliderText_1.Parent = Slider_1
+SliderText_1.BackgroundColor3 = Color3.fromRGB(255,255,255)
+SliderText_1.BackgroundTransparency = 1
+SliderText_1.BorderColor3 = Color3.fromRGB(0,0,0)
+SliderText_1.BorderSizePixel = 0
+SliderText_1.Size = UDim2.new(0, 200,0, 21)
+SliderText_1.Font = Enum.Font.Gotham
+SliderText_1.Text = "Slider"
+SliderText_1.TextColor3 = Color3.fromRGB(172,114,125)
+SliderText_1.TextSize = 13
+SliderText_1.TextXAlignment = Enum.TextXAlignment.Left
+
+UIPadding_9.Parent = SliderText_1
+UIPadding_9.PaddingLeft = UDim.new(0,9)
+
+SliderValue_1.Name = "SliderValue"
+SliderValue_1.Parent = Slider_1
+SliderValue_1.BackgroundColor3 = Color3.fromRGB(255,255,255)
+SliderValue_1.BackgroundTransparency = 1
+SliderValue_1.BorderColor3 = Color3.fromRGB(0,0,0)
+SliderValue_1.BorderSizePixel = 0
+SliderValue_1.Position = UDim2.new(0.732824445, 0,0, 0)
+SliderValue_1.Size = UDim2.new(0, 70,0, 21)
+SliderValue_1.Font = Enum.Font.Gotham
+SliderValue_1.Text = "33"
+SliderValue_1.TextColor3 = Color3.fromRGB(172,114,125)
+SliderValue_1.TextSize = 13
+SliderValue_1.TextXAlignment = Enum.TextXAlignment.Right
+
+UIPadding_10.Parent = SliderValue_1
+UIPadding_10.PaddingRight = UDim.new(0,3)
+
+SliderHolder_1.Name = "SliderHolder"
+SliderHolder_1.Parent = Slider_1
+SliderHolder_1.BackgroundColor3 = Color3.fromRGB(96,73,78)
+SliderHolder_1.BackgroundTransparency = 0.8999999761581421
+SliderHolder_1.BorderColor3 = Color3.fromRGB(0,0,0)
+SliderHolder_1.BorderSizePixel = 0
+SliderHolder_1.Position = UDim2.new(0.0340000018, 0,0.569999993, 0)
+SliderHolder_1.Size = UDim2.new(0, 250,0, 13)
+
+SliderBtn_1.Name = "SliderBtn"
+SliderBtn_1.Parent = SliderHolder_1
+SliderBtn_1.Active = true
+SliderBtn_1.BackgroundColor3 = Color3.fromRGB(255,255,255)
+SliderBtn_1.BackgroundTransparency = 1
+SliderBtn_1.BorderColor3 = Color3.fromRGB(0,0,0)
+SliderBtn_1.BorderSizePixel = 0
+SliderBtn_1.Size = UDim2.new(0, 250,0, 13)
+SliderBtn_1.ClipsDescendants = true
+SliderBtn_1.Font = Enum.Font.SourceSans
+SliderBtn_1.Text = ""
+SliderBtn_1.TextSize = 14
+
+SliderInner_1.Name = "SliderInner"
+SliderInner_1.Parent = SliderBtn_1
+SliderInner_1.BackgroundColor3 = Color3.fromRGB(78,65,67)
+SliderInner_1.BorderColor3 = Color3.fromRGB(0,0,0)
+SliderInner_1.BorderSizePixel = 0
+SliderInner_1.Size = UDim2.new(0, 88,0, 13)
+
+UICorner_4.Parent = SliderInner_1
+
+UICorner_5.Parent = SliderHolder_1
