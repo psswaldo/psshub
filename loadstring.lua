7fc645067fa4522190d498e24233d082b513d5c6
psswaldo <152012526+psswaldo@users.noreply.github.com>
2023-12-12 08:14:43 +0100

    Add files via upload

---
+++ loadstring.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 3101667897 or game.PlaceId == 3276265788 or game.PlaceId == 3232996272 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/psswaldo/psshub/main/legendsofspeed.lua"))()
+end
