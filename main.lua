3f19389d1844a0b080daaa5230f093fe6b363379
psswaldo <152012526+psswaldo@users.noreply.github.com>
2023-12-12 08:16:02 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+-- By Whacky Speed, dont skid, share, steal, copy, or other sort of things!
+
 if game.PlaceId == 3101667897 or game.PlaceId == 3276265788 or game.PlaceId == 3232996272 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/psswaldo/psshub/main/legendsofspeed.lua"))()
 end
