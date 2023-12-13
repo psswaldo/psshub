6e1f5ce60d5e1dc448bcc8a438d14c5c9779bbf5
psswaldo <152012526+psswaldo@users.noreply.github.com>
2023-12-13 18:54:13 +0100

    Delete main.lua

--- main.lua
+++
@@ -1,5 +0,0 @@
--- By Whacky Speed, dont skid, share, steal, copy, or other sort of things!
-
-if game.PlaceId == 3101667897 or game.PlaceId == 3276265788 or game.PlaceId == 3232996272 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/psswaldo/psshub/main/legendsofspeed.lua"))()
-end
